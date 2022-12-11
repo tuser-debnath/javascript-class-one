0437d2b737dad830f6f9249a4ab024981da9eae4
Tuser Debnath <tuserdebnath.bd@gmail.com>
2022-12-11 20:41:40 +0600

    JS Class One Completed

---
+++ script.js
@@ -0,0 +1,18 @@
+// alert("Hello World");
+
+let place = "Bangladesh";
+if (place === "Bangladesh") alert("Hello World!");
+
+console.log(5 + 9);
+
+let firstName = "Tuser";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+console.log("Tuser Debnath");
